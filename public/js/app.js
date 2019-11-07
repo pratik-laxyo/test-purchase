726c9c5107b193fd78dfc3d433721f3028ec3cf2
pratik-laxyo <pratik@laxyo.org>
2019-11-07 17:18:58 +0530

    laxyo-purchase-module

--- public/js/app.js
+++ public/js/app.js
@@ -36985,8 +36985,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! D:\laragon\www\laravel6-starter-kit\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! D:\laragon\www\laravel6-starter-kit\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/dev2/new_clone/laravel6-starter-kit/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/dev2/new_clone/laravel6-starter-kit/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
